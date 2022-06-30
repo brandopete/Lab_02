0ea3f39c69cdb630844d27afb9170b6e2150ee0f
brandopete <40000216+brandopete@users.noreply.github.com>
2022-06-29 22:45:43 -0600

    added main

---
+++ Navigation.h
@@ -0,0 +1,8 @@
+//
+// Created by Brandon Peters on 6/29/2022.
+//
+
+#ifndef LAB_02_NAVIGATION_H
+#define LAB_02_NAVIGATION_H
+
+#endif //LAB_02_NAVIGATION_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,44 @@
-//
-// Created by Brandon Peters on 6/29/2022.
-//
 #include <iostream>
+#include <string>
+#include <queue>
+#include "NavigationInterface.h"
+#include "Navigation.h"
 
-int main(int argc, char* argv[]){
-    std::cout << "HELLO world!" << std::endl;
+using namespace std;
+
+extern void writeImage2File(queue<int> thePath, string _mapName);
+
+// ********************************************
+//
+//  ./navigate [map] [startInd] [endInd]
+//
+// ********************************************
+int main(int argc, char *argv[]) {
+
+    // process the inputs from the command line
+    if (argc < 4) {
+        cout << "Not enough parameters" << endl;
+        return 0;
+    }
+
+    string mapName = argv[1];
+    int startInd = stoi(argv[2]);
+    int endInd = stoi(argv[3]);
+
+    cout << "Solving map " << mapName << " from Node " << startInd << " to Node " << endInd << endl;
+
+    // instantiate an instance of the class you create (in Navigation.h and Navigation.cpp)
+    NavigationInterface *nav = new Navigation();
+
+    // make calls to functions in the Navigate class
+    nav->readNetwork(mapName);
+    queue<int> thePath = nav->computeShortestPath(startInd, endInd);
+    nav->printPath(thePath);
+
+    // make a call that creats a Map.bmp, which shows the shortest path computed through the road network
+    writeImage2File(thePath, mapName);
+
+    delete nav;
+
     return 0;
 }
