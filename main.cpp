3d4d2f44b4f59536596e847b41a0b57e6eaa2119
brandopete <40000216+brandopete@users.noreply.github.com>
2022-06-30 11:30:26 -0600

    added main

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    cout << "HELLO WORLD";
+    cout << "HELLO world";
 
     string mapName = argv[1];
     int startInd = stoi(argv[2]);
