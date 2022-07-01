35aa7f4682fe27dc58e01fdfc84dad553d9f281b
brandopete <40000216+brandopete@users.noreply.github.com>
2022-06-30 20:17:55 -0600

    added main

--- Navigation.cpp
+++ Navigation.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <string>
 #include <queue>
 #include "Navigation.h"
@@ -6,23 +5,26 @@
 using namespace std;
 
 
-    Navigation::Navigation() {}
-    Navigation::~Navigation() {}
+    Navigation::Navigation() {
+        cout << "In constructor" << endl;
+    }
+    Navigation::~Navigation() {
+        cout << "In destructor" << endl;
+    }
 
-    // This function reads in the contents of the file _mapName.
-    // The function can assume that the contents of the specified conform with the specs given in the README.txt file
-    // Returns true if the file is read in successfully, false otherwise
-    bool Navigation::readNetwork(string _mapName){}
+    bool Navigation::readNetwork(string _mapName){
+        cout << "reading the road network" << endl;
 
-    // This function computes and returns the least-cost path from node indexed _startInd to node indexed as _endInd
-    // for the map that was last read (in the readMap function).  The resulting path must be stored in an integer queue
-    // with each entry in the queue denoting the next node in the path.  First front of the queue should be _startInd
-    // and the back of the queue should be _endInd.
-    // For example, in map1.txt, solveMap(13, 1) should return the shortest path from node 13 to node 1, which is:
-    //      13, 9, 10, 7, 3, 2, 1
-    // You can assume that readNetwork(_mapName) is called before this function and that a path from _startInd to _endInd exists
-    queue<int> Navigation::computeShortestPath(int _startInd, int _endInd){}
+        return false;
+    }
 
-    // prints out to the console the path stored in _path
-    void Navigation::printPath(queue<int> _path){}
-};
+
+    queue<int> Navigation::computeShortestPath(int _startInd, int _endInd){
+        queue<int> thePath;
+        cout << "Not computing shortest path" << endl;
+        return thePath;
+    }
+
+    void Navigation::printPath(queue<int> _path){
+        cout << "I'm not printing stuff yet" << endl;
+    }
